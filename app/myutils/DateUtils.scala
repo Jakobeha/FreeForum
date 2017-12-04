16475591b20f97ffd22799a4ad2422e8604b8b03
jakob <jakobeha2@gmail.com>
2017-12-04 13:45:06 -0500

    Changed lock/unlock date

--- app/myutils/DateUtils.scala
+++ app/myutils/DateUtils.scala
@@ -5,9 +5,9 @@ import java.util.Date
 import java.time.Instant
 
 object DateUtils {
-  val format = new SimpleDateFormat("hh:mm aa, dd/MM/yyyy")
-  val forumUnlockDate: Date = format.parse("1:33 PM, 04/12/2017")
-  val forumLockDate: Date = format.parse("1:45 PM, 04/12/2017")
+  val format = new SimpleDateFormat("hh:mm aa, dd/MM/yyyy zzz")
+  val forumUnlockDate: Date = format.parse("1:45 PM, 04/12/2017 EST")
+  val forumLockDate: Date = format.parse("2:00 PM, 04/12/2017 EST")
 
   def now(): Date = Date.from(Instant.now())
 }
